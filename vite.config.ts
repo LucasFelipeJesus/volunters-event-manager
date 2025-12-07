402d3145827fc4f12589ba33d674aef372c3239e
LucasFelipeJesus <lucasfelipejesus@gmail.com>
2025-12-07 13:40:00 -0300

    ajuste para deply

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/volunters-event/',
+  base: '/volunters-event-manager/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
